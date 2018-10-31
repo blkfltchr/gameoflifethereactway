a99b773f5f4ba0e2f38cb50a106b3630e563b7f8
Blake Fletcher <blkfltchr@gmail.com>
2018-10-31 14:06:23 -0400

    Updates

---
+++ precache-manifest.7bfd15e186a40ea5f745fce69a2b69a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1221a57c0bee953e00df",
+    "url": "/gameoflifethereactway/static/js/runtime~main.1221a57c.js"
+  },
+  {
+    "revision": "53f8762688cb78ff3ec6",
+    "url": "/gameoflifethereactway/static/js/main.53f87626.chunk.js"
+  },
+  {
+    "revision": "843cb8751f1ded95b09d",
+    "url": "/gameoflifethereactway/static/js/1.843cb875.chunk.js"
+  },
+  {
+    "revision": "53f8762688cb78ff3ec6",
+    "url": "/gameoflifethereactway/static/css/main.a8d0fa9d.chunk.css"
+  },
+  {
+    "revision": "d43b6f1c0c70e9f730872d56ea2ba5c4",
+    "url": "/gameoflifethereactway/index.html"
+  }
+];
